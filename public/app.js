cb3c327fe3bdd4ab9a3d185a702db0888b9c9e62
Aurelie Bayre <a.bayre@gmail.com>
2018-04-28 21:16:12 +0200

    docs: ADD todo list

--- public/app.js
+++ public/app.js
@@ -19,8 +19,6 @@ const canvasSpace = `
     Votre navigateur ne supporte pas le Canvas HTML5
     </canvas>
     `
-
-
 renderSpace(canvasSpace)
 const drawingSpace = document.getElementById("drawingSpace")
 
@@ -37,3 +35,15 @@ ball.fillStyle="#1D1075"
 ball.arc(270, 320, 10, 0, 2 * Math.PI);
 ball.fill();
 
+
+//fonctions:
+/*
+* start qui donne toutes les propriétés(emplacements, vélocité)
+* quand tout le jeu fonctionne, faire une fonction restart.
+* pour la partie dev: un autorestart quand la balle est sortie
+* 
+* collision avec les bord du plateau (+ 10 ou moins 10)
+* collision avec les paddles
+* mouvement alétoire de la balle
+* Aurélie déplacement des paddles gestion des touches et vélocité des paddles et collision paddle bord du plateau
+ */
