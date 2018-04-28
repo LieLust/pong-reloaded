9ceb470f6ca2b3776028e2178cad0c9ebb17f40a
Maxence <Maxence.millescamps.macarty@gmail.com>
2018-04-28 20:57:04 +0200

    add ball

--- public/app.js
+++ public/app.js
@@ -4,4 +4,9 @@ const render = html => {
     mainDiv.innerHTML = html
 }
 
-render(`<h1> On joue à Pong?</h1>`)
+render(`<canvas id="drawingSpace" width="540" height="640" style="border: 1px solid #000000;">Votre navigateur ne supporte pas canvas HTML 5</canvas>`)
+const ball = document.getElementById("drawingSpace");
+const ctx = ball.getContext("2d");
+ctx.fillStyle="#1D1075"
+ctx.arc(270, 320, 10, 0, 2 * Math.PI);
+ctx.fill();
